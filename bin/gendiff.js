83dce535f0ccbbcd39de235f91136a226ce2c064
Yaroslav Mikhaylov <sponaproz@gmail.com>
2022-01-25 16:02:37 +0300

    step #2: args and option -f added to cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const { Command } = require('commander');
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .arguments('<filepath1> <filepath2>')
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
